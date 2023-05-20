d218dd44ed8281567bbe5b26fc95904c337cc78e
Olabode Adebayo <webframes@gmail.com>
2023-05-20 13:39:40 +0100

    kunles first class with javascript

---
+++ demo2/main.js
@@ -0,0 +1,5 @@
+window.onload=function(){
+    //alert("Hello world")
+    var name = prompt("Please enter your name?");
+    alert("Your name is "+name)
+}
---
+++ demo3/main.js
@@ -0,0 +1,3 @@
+window.onload=function(){
+   document.getElementById("content").innerHTML="Hello Kunle";
+}
